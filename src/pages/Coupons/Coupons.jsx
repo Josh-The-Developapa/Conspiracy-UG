575091ef92f4ca16310ff27d212c00f87d9ddd94
Joshua Mukisa <kiryowajoshua22@gmail.com>
2024-04-12 12:01:42 +0300

    Committed

--- src/pages/Coupons/Coupons.jsx
+++ src/pages/Coupons/Coupons.jsx
@@ -49,39 +49,6 @@ function Coupons() {
       });
   }, []);
 
-  const handleMarkComplete = async (index) => {
-    const updatedOrders = [...orders];
-    updatedOrders[index].status = 'Completed';
-
-    const order = updatedOrders[index];
-
-    try {
-      const res = await fetch(
-        `https://conspiracy-67f09-default-rtdb.firebaseio.com/orders/${updatedOrders[index].ID}.json`,
-        // 'https://conspiracy-67f09-default-rtdb.firebaseio.com/orders/-NurfDkwqmqqqduPO729'
-        {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            cartItems: order.cartItems,
-            address: order.address,
-            totalShirts: order.cartItems.totalQuantity,
-            totalCost: order.cartItems.totalCost,
-            coupon: order.coupon,
-            date: order.eatTimeString,
-            status: 'Completed',
-          }),
-        }
-      );
-      setOrders(updatedOrders);
-    } catch (error) {
-      console.error('Error updating order status:', error);
-      // Handle error, e.g., display error message to user
-    }
-  };
-
   return (
     <div
       style={{
@@ -107,7 +74,7 @@ function Coupons() {
             textAlign: 'center',
           }}
         >
-          Greetings and welcome to the Conspiracy UG orders page. This platform
+          Greetings and welcome to the Conspiracy UG coupons page. This platform
           is dedicated to facilitating orders for our esteemed shareholders,
           providing an exclusive and private space tailored to their needs.
           Access to this page is restricted solely to authorized shareholders of
@@ -115,103 +82,7 @@ function Coupons() {
           transactions.
         </p>
         <h2 className="error-message">{error}</h2>
-        <div className="orders-list">
-          {!loading ? (
-            orders.map((order, index) => (
-              <div key={index} className="order-item">
-                <h1 className="page-title">Receipt</h1>
-                {order.status == 'Completed' ? (
-                  <button
-                    style={{ color: 'white', backgroundColor: 'green' }}
-                    disabled
-                  >
-                    Completed
-                  </button>
-                ) : (
-                  <button
-                    style={{ color: 'white', backgroundColor: '#004cff' }}
-                    onClick={() => handleMarkComplete(index)}
-                  >
-                    Pending
-                  </button>
-                )}
-                <p className="order-details">
-                  <p className="order-details">
-                    <span className="order-label">Full Name:</span>{' '}
-                    {order.address.fullName} <br />
-                    {/* <br /> */}
-                    <span className="order-label">Phone Number:</span>{' '}
-                    {order.address.phoneNumber} <br />
-                    {/* <br /> */}
-                    <span className="order-label">City:</span>{' '}
-                    {order.address.cityTown} <br />
-                    {/* <br /> */}
-                    <span className="order-label">Street:</span>{' '}
-                    {order.address.street} <br />
-                    {/* <br /> */}
-                    <span className="order-label">
-                      Total number of shirts:
-                    </span>{' '}
-                    {order.totalShirts} <br />
-                    {/* <br /> */}
-                    <span className="order-label">Date:</span>{' '}
-                    {`${order.date.split()}`} <br />
-                    {/* <br /> */}
-                    <span className="order-label">Coupon:</span>{' '}
-                    {order.coupon.trim() == '' ? 'None' : order.coupon}
-                    <br />
-                    {/* <br /> */}
-                    {/* <span className="order-label">STATUS:</span> <br />
-                    <h2 style={{ margin: 0, marginBottom: '10px' }}>
-                      {order.status.toUpperCase()}
-                    </h2> */}
-                    {/* <br /> */}
-                    <span className="order-label">TOTAL COST:</span> <br />
-                    {/* <br /> */}
-                    <h2
-                      style={{ margin: 0 }}
-                    >{`${order.totalCost.toLocaleString(
-                      'en-US'
-                    )} UGX`}</h2>{' '}
-                    {/* <br />
-                  <br /> */}
-                  </p>
-                </p>
-                <h3 className="shirts-title">Shirts Ordered:</h3>
-                <div className="shirts-ordered">
-                  {Object.values(order.cartItems).map((shirt, shirtIndex) => (
-                    <div key={shirtIndex} className="shirt-item">
-                      <img
-                        src={`/src/assets/${shirt.image}`}
-                        // src={ModelsPic}
-                        alt={shirt.title}
-                        className="shirt-image"
-                      />
-                      <div
-                        style={{
-                          display: 'flex',
-                          flexDirection: 'column',
-                          justifyContent: 'center',
-                          alignItems: 'center',
-                          marginLeft: '10px',
-                        }}
-                      >
-                        {`${shirt.title}   `}
-                        <br />
-                        Price: {`${shirt.price.toLocaleString('en-US')} UGX`}
-                        <br />
-                        Quantity: {shirt.quantity}
-                        <br />
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            ))
-          ) : (
-            <ImSpinner9 className="loading-icon" />
-          )}
-        </div>
+        <div className="orders-list"></div>
       </div>
       <Footer />
     </div>
